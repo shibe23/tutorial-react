3ab94c0244dd0967dfb0f3db5c915d79e9f75437
shibe23 <shibe23.info@gmail.com>
2018-07-24 23:40:49 +0900

    Functional Components: Squareをfunctional componentsに変更

--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-       className="square"
-       onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return(
+    <button className="square" onClick={props.onClick}>{props.value}</button>
+  )
 }
 
 class Board extends React.Component {
@@ -21,6 +14,12 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
     }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
   renderSquare(i) {
     return (
